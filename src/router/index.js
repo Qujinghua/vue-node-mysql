d86b456f0e8be5fa801362f86a6fbce89d435289
Qujignhua <893510847@qq.com>
2017-12-06 18:37:15 +0800

    路由

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import store from '../store/index.js'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'Hello',
+      component(resolve) {
+        require.ensure(['@/components/Hello.vue'], () => {
+          resolve(require('@/components/Hello.vue'));
+        })
+      },
+      //设置meta字段，表示该字段需要验证
+      meta: {
+        requireAuth: true
+      }
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component(resolve) {
+        require.ensure(['@/components/login/register.vue'], () => {
+          resolve(require('@/components/login/register.vue'))
+        })
+      }
+    }
+
+  ]
+})
