1b4a900236111aaed83f8d843f508ec8b8ad9f29
Qujignhua <893510847@qq.com>
2017-12-06 18:36:05 +0800

    vuex登录状态

---
+++ src/store/action.js
@@ -0,0 +1,16 @@
+import api from '../axios'
+import Vue from 'vue'
+import router from '../router/index'
+import * as types from './types'
+
+export default {
+  UserLogin({commit}, data) {
+    commit(types.LOGIN, data)
+  },
+  UserLogout({commit}) {
+    commit(types.LOGOUT)
+  },
+  UserName({commit}, data) {
+    commit(types.USERNAME, data)
+  }
+}
