e1429d9723366967c5ab2ac44dffa926b1795706
qujinghua <893510847@qq.com>
2018-01-15 18:24:58 +0800

    首页

--- src/components/homepage/homeheader/index.vue
+++ src/components/homepage/homeheader/index.vue
@@ -1,18 +1,30 @@
 <template>
-  <div>欢迎{{ username }}</div>
+  <div class="header">
+    <div class="header-logo"></div>
+    欢迎{{ username }}
+  </div>
 </template>
 <script>
 export default {
   data () {
     return {
-      
+
     }
   },
   computed: {
     username () {
-      return this.$store.state.username
+      return localStorage.getItem('username')
     }
   }
 }
 </script>
+<style lang="scss" scoped>
+.header {
+  width: 100%;
+  height: 60px;
+  background: #434A50;
+  color: #fff;
+}
+</style>
+
 
