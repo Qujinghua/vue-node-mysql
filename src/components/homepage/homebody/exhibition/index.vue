5225ccb1c6d2e90215a55935f273a6985a0b8db5
qujinghua <893510847@qq.com>
2018-02-06 19:20:27 +0800

    网上展厅

--- src/components/homepage/homebody/exhibition/index.vue
+++ src/components/homepage/homebody/exhibition/index.vue
@@ -13,26 +13,36 @@
               <span>产品大类</span>
               <ul>
                 <li @click="bigCList('0')" :class="{caseChangeClick:choose.big_name==0}">不限</li>
-                <li v-for="(item,index) in bigCLists" :class="{caseChangeClick:choose.big_name==item.id}" @click="bigCList(item.id)">{{item.big_name}}</li>
+                <li v-for="(item,index) in bigCLists" :class="{caseChangeClick:choose.big_name==item.id}" @click="bigCList(item.id, item.big_name)">{{item.big_name}}</li>
               </ul>
             </div>
+            <div class="ex-menu-classify-content-dashed"></div>
             <div class="ex-menu-classify-content-smallC">
               <span>产品小类</span>
               <ul>
                 <li @click="smallCList('0')" :class="{caseChangeClick:choose.big_name==0}">不限</li>
-                <li v-for="(item,index) in smallCLists" :class="{caseChangeClick:choose.small_name==item.id}" @click="smallCList(item.id)">{{item.small_name}}</li>
+                <li v-for="(item,index) in smallCLists" :class="{caseChangeClick:choose.small_name==item.id}" @click="smallCList(item.id, item.small_name)">{{item.small_name}}</li>
               </ul>
             </div>
-            <div class="ex-menu-classify-content-smallC">
+            <div class="ex-menu-classify-content-dashed"></div>
+            <div class="ex-menu-classify-content-brand">
               <span>产品系列</span>
               <ul>
                 <li @click="brandLists('0')" :class="{caseChangeClick:choose.brand_name==0}">不限</li>
-                <li v-for="(item,index) in brandLists" :class="{caseChangeClick:choose.brand_name==item.id}" @click="smallCList(item.id)">{{item.brand_name}}</li>
+                <li v-for="(item,index) in brandLists" :class="{caseChangeClick:choose.brand_name==item.id}" @click="brandList(item.id, item.brand_name)">{{item.brand_name}}</li>
               </ul>
             </div>
           </div>
-        </div>  
-        <div class="ex-menu-choose">已选条件</div>  
+        </div>
+        <div class="ex-menu-choose" v-if="choose.big_name!=0||choose.small_name!=0||choose.brand_name!=0">
+          <span>已选条件</span>
+          <ul>
+            <li v-if="choose.big_name!=0"><span>{{chooseShow.big_name}}</span><span @click="deleteBigC"><i class="el-icon-close"></i></span></li>
+            <li v-if="choose.small_name!=0"><span>{{chooseShow.small_name}}</span><span @click="deleteSmallC"><i class="el-icon-close"></i></span></li>
+            <li v-if="choose.brand_name!=0"><span>{{chooseShow.brand_name}}</span><span @click="deleteBrand"><i class="el-icon-close"></i></span></li>
+          </ul>
+          <span @click="emptyChoose"><i class="el-icon-delete"></i><span>清空条件</span></span>
+        </div>
       </div>
       <div class="ex-"></div>
     </div>
@@ -87,36 +97,171 @@ export default {
         {id:1,brand_name:'KANO'},
         {id:2,brand_name:'YESCOCO'}
       ],
-      choose: {big_name:'',small_name:'',product:'',city:''}, 
+      choose: {big_name: 0, small_name: 0, brand_name: 0, city: ''},
+      chooseShow: {big_name: '', small_name: '', brand_name: '', city: ''},
     }
   },
   mounted () {
 
   },
   methods: {
-    bigCList () {
+    bigCList (id, name) {
+      this.choose.big_name = id
+      this.chooseShow.big_name = name
+    },
+    smallCList (id, name) {
+      this.choose.small_name = id
+      this.chooseShow.small_name = name
+    },
+    brandList (id, name) {
+      this.choose.brand_name = id
+      this.chooseShow.brand_name = name
+    },
+    deleteBigC () {
 
     },
-    bigCList () {
+    deleteSmallC () {
+
+    },
+    deleteBrand () {
+
+    },
+    emptyChoose () {
 
     }
+
   }
 }
 </script>
 <style lang="scss" scoped>
 .caseChangeClick {
-
+  background: #409EFF;
+  color: #FFFFFF;
+  padding-right: 5px;
+  padding-left: 5px;
+  height: 24px;
+  text-align: center;
+  line-height: 24px;
+  border-radius: 3px;
 }
 .ex {
+  margin-top: 15px;
   &-menu {
     &-classify {
+      border-top: 2px solid #409EFF;
+      border-right: 1px solid #e6e6e6;
+      border-bottom: 1px solid #e6e6e6;
+      border-left: 1px solid #e6e6e6;
+      border-bottom-left-radius: 3px;
+      border-bottom-right-radius: 3px;
+      padding-left: 10px;
+      padding-right: 10px;
       &-content {
-        & ul li {
-          list-style-type: none;
-          float: left;
+        &-dashed {
+          height: 0;
+          width: 100%;
+          margin: 0 auto;
+          border-top: 1px dashed #d8d8d8;
+        }
+        & ul {
+          overflow: auto;
+          & li {
+            list-style-type: none;
+            float: left;
+            cursor: pointer;
+            margin-left: 5px;
+            margin-bottom: 5px;
+            padding-right: 5px;
+            padding-left: 5px;
+            height: 24px;
+            text-align: center;
+            line-height: 24px;
+            border-radius: 3px;
+            &:hover {
+              transition: 0.2s;
+              background: #409EFF;
+              color: #FFFFFF;
+            }
+          }
+        }
+        &-bigC,&-smallC,&-brand {
+          margin-top: 10px;
+          margin-bottom: 5px;
+          overflow: auto;
+          &>span {
+            display: inline-block;
+            margin-right: 5px;
+            float: left;
+            padding-right: 5px;
+            padding-left: 5px;
+            height: 24px;
+            text-align: center;
+            line-height: 24px;
+            border-radius: 3px;
+            background: #EBEEF5;
+          }
         }
         &-bigC {
 
+        }
+      }
+    }
+    &-choose {
+      margin-top: 15px;
+      border: 1px solid #e6e6e6;
+      border-radius: 3px;
+      padding: 10px;
+      display: flex;
+      justify-content: flex-start;
+      &>span:nth-child(1) {
+        margin-right: 10px;
+        height: 24px;
+        line-height: 24px;
+        border-radius: 3px;
+        padding-left: 5px;
+        padding-right: 5px;
+        background: #EBEEF5;
+      }
+      //清空条件
+      &>span:nth-child(3) {
+        cursor: pointer;
+        margin-left: 10px;
+        height: 24px;
+        color: #409EFF;
+        line-height: 24px;
+        & span {
+          margin-right: 2px;
+        }
+      }
+      & ul {
+        overflow: auto;
+        & li {
+          list-style-type: none;
+          float: left;
+          margin-right: 5px;
+          border: 1px solid #409EFF;
+          border-radius: 3px;
+          & span {
+            display: inline-block;
+            float: left;
+            &:nth-child(1) {
+              height: 24px;
+              line-height: 24px;
+              padding-left: 5px;
+              padding-right: 5px;
+            }
+            &:nth-child(2) {
+              cursor: pointer;
+              width: 24px;
+              text-align: center;
+              height: 24px;
+              line-height: 24px;
+              background: #409EFF;
+              & i {
+                color: #FFFFFF;
+              }
+            }
+          }
         }
       }
     }
