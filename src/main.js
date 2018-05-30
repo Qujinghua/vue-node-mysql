319aa43d9de7dd93d835aba7716d08a5801e03e6
unknown <893510847@qq.com>
2018-05-31 06:43:38 +0800

    ..

--- src/main.js
+++ src/main.js
@@ -8,12 +8,9 @@ import 'element-ui/lib/theme-chalk/index.css'
 import axios from './axios'
 import store from './store/index.js'
 import echarts from 'echarts'
-
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.prototype.$echarts = echarts
-
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   axios,
